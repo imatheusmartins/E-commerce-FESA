83a3795264575584b317421dabbcb4a94a157d6e
Matheus Martins <matheusjayaly@gmail.com>
2024-05-18 14:30:58 -0300

    Exclusão do teste

--- Models/PadraoViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace N1.Models
-{
-    public abstract class PadraoViewModel
-    {
-        public virtual int Id { get; set; }
-    }
-}
--- Models/PedidoViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace N1.Models
-{
-    public abstract class PedidoViewModel : PadraoViewModel
-    {
-        public DateTime Data { get; set; }
-    }
-}
