86ac35e69d09e878ed42cca2e60933283aba48a0
MTHCunha <mthcunha2003@gmail.com>
2024-05-22 00:54:16 -0300

    Ultimo commit

---
+++ EcommerceLicenca/EcommerceLicenca/Controllers/SobreController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceLicenca.Controllers
+{
+    public class SobreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
